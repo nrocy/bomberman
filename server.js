efb06c34096186c993fc16c87b93c6991b13fa4c
Paul King <cronald@gmail.com>
2011-06-15 14:09:49 +0100

    Add player <-> crafty entity stub

--- server.js
+++ server.js
@@ -2,26 +2,20 @@
 var UPDATES_PER_SEC = 1;
 var json = JSON.stringify;
 
-var game_state = [
-  /*
-  { object_id: 1, type: "player", pos: {x: 1, y: 1}, created_at: 1, dead: false },
-  { object_id: 2, type: "player", pos: {x: 2, y: 2}, created_at: 2, dead: false },
-  { object_id: 1, type: "bomb",   pos: {x: 2, y: 3}, created_at: 4, dead: false }
-  */
-];
+var game_state = [];
 
 var http = require("http"),
-  io = require("socket.io"),
-  sys = require("sys"),
-  nodestatic = require("node-static");
+	io = require("socket.io"),
+	sys = require("sys"),
+	nodestatic = require("node-static");
 
 var static_server = new nodestatic.Server(".");
 
 var server = http.createServer(function(req, res) {
-  var url = require("url").parse(req.url);
-  var pathfile = url.pathname;
+	var url = require("url").parse(req.url);
+	var pathfile = url.pathname;
 
-  static_server.serve(req, res);
+	static_server.serve(req, res);
 });
 
 server.listen(1234);
@@ -29,53 +23,53 @@ server.listen(1234);
 var socket = io.listen(server);
 socket.on("connection", function(client) {
 
-  client.on("disconnect", function() {
-    for( var i = 0 ; i < game_state.length ; i++ ) {
-      if( game_state[i].object_id === client.sessionId ) {
-        game_state.splice(i, 1);
-      }
-    }
-  });
+	client.on("disconnect", function() {
+		for( var i = 0 ; i < game_state.length ; i++ ) {
+			if( game_state[i].object_id === client.sessionId ) {
+				game_state.splice(i, 1);
+			}
+		}
+	});
 
-  game_state.push({
-    object_id: client.sessionId, 
-    type: "player", 
-    pos: {x: 1, y: 1}, 
-    created_at: 1, 
-    dead: false
-  });
+	game_state.push({
+		object_id: client.sessionId, 
+		type: "player", 
+		pos: {x: 1, y: 1}, 
+		created_at: 1, 
+		dead: false
+	});
 
-  client.send(json(game_state));
+	client.send(json(game_state));
 
-  client.on("message", function(msg) {
-    var player;
-    
-    for( var i = 0 ; i < game_state.length ; i++ ) {
-      if( game_state[i].object_id === client.sessionId ) {
-        player = game_state[i];
-        break;
-      }
-    }
+	client.on("message", function(msg) {
+		var player;
+		
+		for( var i = 0 ; i < game_state.length ; i++ ) {
+			if( game_state[i].object_id === client.sessionId ) {
+				player = game_state[i];
+				break;
+			}
+		}
 
-    if( player ) {
-      if( msg.up ) {
-        player.pos.y--;
-      }
-      if( msg.down ) {
-        player.pos.y++;
-      }
-      if( msg.left ) {
-        player.pos.x--;
-      }
-      if( msg.right ) {
-        player.pos.x++;
-      }
-      if( msg.space ) {
-      }
-      
-      socket.broadcast(json(game_state));
-    }
-  });
+		if( player ) {
+			if( msg.up ) {
+				player.pos.y--;
+			}
+			if( msg.down ) {
+				player.pos.y++;
+			}
+			if( msg.left ) {
+				player.pos.x--;
+			}
+			if( msg.right ) {
+				player.pos.x++;
+			}
+			if( msg.space ) {
+			}
+			
+			socket.broadcast(json(game_state));
+		}
+	});
 
 });
 
--- static/js/client.js
+++ static/js/client.js
@@ -1,102 +1,124 @@
+
+var game_objects = {};
+
 window.onload = function() {
-  var SCREEN_WIDTH = 400, SCREEN_HEIGHT = 400;
-  var socket = new io.Socket(location.hostname, {port: 1234});
+	var SCREEN_WIDTH = 400, SCREEN_HEIGHT = 400;
+	var socket = new io.Socket(location.hostname, {port: 1234});
 
-  Crafty.init(SCREEN_WIDTH, SCREEN_HEIGHT);
-  Crafty.canvas();
+	Crafty.init(SCREEN_WIDTH, SCREEN_HEIGHT);
+	Crafty.canvas();
 
-  Crafty.stage.elem.style.background = "#eee";
+	Crafty.stage.elem.style.background = "#eee";
 
-  Crafty.scene("game", function() {
-    Crafty.e("player, ServerControls, controls")
-      .attr({
-        x: 0,
-        y: 0
-      })
-      .bind("enterframe", function() {
-      });
-  });
-
-  socket.connect();
-
-  socket.on("connect", function() {
-    socket.send({up: true});
-  });
-
-  socket.on("message", function(msg) {
-    var game_state = JSON.parse(msg);
-    for( var i = 0 ; i < game_state.length ; i++ ) {
-      var obj = game_state[i];
-      console.log(obj.type + " " + obj.object_id + ": (" + obj.pos.x + "," + obj.pos.y + ")");
-    }
-  });
-
-  socket.on("disconnect", function() {
-    console.log("disconnect!");
-  });
-
-  Crafty.c ("ServerControls", {
-			init : function () {
-
-
-
-				var __controls = {
-						left : false,
-						right : false,
-						up : false,
-						down: false,
-						space : false
-				};
-
-
-				if (!this.has('controls')) {
-					this.addComponent ('controls');
+	Crafty.scene("game", function() {
+		Crafty.c("bomberman", {
+			init: function() {
+				if( !this.has("color") ) {
+					this.addComponent("color");
 				}
-				this.bind ("keyup", function (e) {
-						if (e.keyCode == Crafty.keys.UP_ARROW) {
-							__controls.up = false;
-						}
-						if (e.keyCode == Crafty.keys.DOWN_ARROW) {
-							__controls.down = false;
-						}
-						if (e.keyCode == Crafty.keys.LEFT_ARROW) {
-							__controls.left = false;
-						}
-						if (e.keyCode == Crafty.keys.RIGHT_ARROW) {
-							__controls.right = false;
-						}
-						if (e.keyCode == Crafty.keys.SPACE) {
-							__controls.space = false;
-						}
-				});
-				this.bind("keydown", function (e) {
-					if (e.keyCode == Crafty.keys.UP_ARROW) {
-						__controls.up = true;
-					}
-					if (e.keyCode == Crafty.keys.DOWN_ARROW) {
-						__controls.down = true;
-					}
-					if (e.keyCode == Crafty.keys.LEFT_ARROW) {
-						__controls.left = true;
-					}
-					if (e.keyCode == Crafty.keys.RIGHT_ARROW) {
-						__controls.right = true;
-					}
-					if (e.keyCode == Crafty.keys.SPACE) {
-						__controls.space = true;
-					}
-				});
 
-
-				this.bind ("enterframe", function (e) {
-					socket.send (__controls);
-				});
-				return this;
+				this.attr({
+					x: 0,
+					y: 0,
+					w: 10,
+					h: 10
+				})
+				.color("blue");
 			}
+		});
+
+		Crafty.e("player, bomberman, ServerControls, controls")
+		.attr({
+			x: 0,
+			y: 0,
+			session_id: null
+		})
+		.bind("enterframe", function() {
+		});
+	});
+
+	socket.connect();
+
+	socket.on("connect", function() {
+		socket.send({up: true});
+	});
+
+	socket.on("message", function(msg) {
+		var game_state = JSON.parse(msg);
+		for( var i = 0 ; i < game_state.length ; i++ ) {
+			var obj = game_state[i];
+
+			if( game_objects[obj.object_id] ) {
+				console.log(".");
+			} else {
+				game_objects[obj.object_id] = {};
+				console.log("create " + obj.object_id);
+			}
+		}
+	});
+
+	socket.on("disconnect", function() {
+		console.log("disconnect!");
+	});
+
+	Crafty.c ("ServerControls", {
+		init : function () {
+			var __controls = {
+				left : false,
+				right : false,
+				up : false,
+				down: false,
+				space : false
+			};
+
+			if (!this.has('controls')) {
+				this.addComponent ('controls');
+			}
+			this.bind ("keyup", function (e) {
+				if (e.keyCode == Crafty.keys.UP_ARROW) {
+					__controls.up = false;
+				}
+				if (e.keyCode == Crafty.keys.DOWN_ARROW) {
+					__controls.down = false;
+				}
+				if (e.keyCode == Crafty.keys.LEFT_ARROW) {
+					__controls.left = false;
+				}
+				if (e.keyCode == Crafty.keys.RIGHT_ARROW) {
+					__controls.right = false;
+				}
+				if (e.keyCode == Crafty.keys.SPACE) {
+					__controls.space = false;
+				}
+			});
+			this.bind("keydown", function (e) {
+				if (e.keyCode == Crafty.keys.UP_ARROW) {
+					__controls.up = true;
+				}
+				if (e.keyCode == Crafty.keys.DOWN_ARROW) {
+					__controls.down = true;
+				}
+				if (e.keyCode == Crafty.keys.LEFT_ARROW) {
+					__controls.left = true;
+				}
+				if (e.keyCode == Crafty.keys.RIGHT_ARROW) {
+					__controls.right = true;
+				}
+				if (e.keyCode == Crafty.keys.SPACE) {
+					__controls.space = true;
+				}
+			});
+
+
+			this.bind ("enterframe", function (e) {
+				socket.send (__controls);
+			});
+			return this;
+		}
 	});
 
 
-  Crafty.scene("game");
+	Crafty.scene("game");
 };
 
 
