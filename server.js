975f74d15f8229d38cb3c0a1d3d8f69322706a8d
Paul King <cronald@gmail.com>
2011-06-15 13:08:46 +0100

    Add player object to client

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 var UPDATES_PER_SEC = 1;
 var json = JSON.stringify;
 
+var game_state = [
+  { object_id: 1, type: "player", pos: {x: 1, y: 1}, created_at: 1, dead: false },
+  { object_id: 2, type: "player", pos: {x: 2, y: 2}, created_at: 2, dead: false },
+  { object_id: 1, type: "bomb",   pos: {x: 2, y: 3}, created_at: 4, dead: false }
+];
+
 var http = require("http"),
   io = require("socket.io"),
   sys = require("sys"),
@@ -21,16 +27,12 @@ server.listen(1234);
 var socket = io.listen(server);
 socket.on("connection", function(client) {
 
-  var game_state = [
-    { object_id: 1, type: "player", pos: {x: 1, y: 1}, created_at: 1, dead: false },
-    { object_id: 2, type: "player", pos: {x: 2, y: 2}, created_at: 2, dead: false },
-    { object_id: 1, type: "bomb",   pos: {x: 2, y: 3}, created_at: 4, dead: false }
-  ];
-
   client.send(json(game_state));
 
   client.on("message", function(msg) {
-    socket.broadcast(json(game_state));
+    if( msg.up ) {
+    }
+    //socket.broadcast(json(game_state));
   });
 
 });
--- static/js/client.js
+++ static/js/client.js
@@ -1,6 +1,7 @@
 
 window.onload = function() {
   var SCREEN_WIDTH = 400, SCREEN_HEIGHT = 400;
+  var socket = new io.Socket(location.hostname, {port: 1234});
 
   Crafty.init(SCREEN_WIDTH, SCREEN_HEIGHT);
   Crafty.canvas();
@@ -8,21 +9,30 @@ window.onload = function() {
   Crafty.stage.elem.style.background = "#eee";
 
   Crafty.scene("game", function() {
-    console.log("crafty start");
+    Crafty.e("player")
+      .attr({
+        x: 0,
+        y: 0
+      })
+      .bind("enterframe", function() {
+        socket.send({up: true});
+      })
   });
 
-  var socket = new io.Socket(location.hostname, {port: 1234});
-
   socket.connect();
+
   socket.on("connect", function() {
     console.log("connect!");
   });
+
   socket.on("message", function(msg) {
     var game_state = JSON.parse(msg);
     for( var i = 0 ; i < game_state.length ; i++ ) {
-      console.log(game_state[i].type);
+      var obj = game_state[i];
+      console.log(obj.type + " " + obj.object_id + ": (" + obj.pos.x + "," + obj.pos.y + ")");
     }
   });
+
   socket.on("disconnect", function() {
     console.log("disconnect!");
   });
