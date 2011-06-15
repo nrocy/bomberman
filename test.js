40e5c8386135eb5ccdd4a6d1b7cfe59cc072da7e
Paul King and Clinton Montague <cronald+nrocy+slightlymore@gmail.com>
2011-06-15 11:11:33 +0100

    Fix completely wrong index.html, node now serving static content

--- test.js
+++ test.js
@@ -2,10 +2,20 @@
 var http = require("http"),
   io = require("socket.io"),
   sys = require("sys"),
+  nodestatic = require("node-static");
 
-server = http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.end("<h1>Hello, world!</h1>");
+var static_server = new nodestatic.Server(".");
+
+var server = http.createServer(function(req, res) {
+  var url = require("url").parse(req.url);
+  var pathfile = url.pathname;
+
+  if( pathfile.search(/^\/static\//) === 0 ) {
+    static_server.serve(req, res);
+  } else {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("<h1>Hello, world!</h1>");
+  }
 });
 
 server.listen(1234);
