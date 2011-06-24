d1a3b997b496d51ddd43b440fbd6c909c585a6f3
Paul King <cronald@gmail.com>
2011-06-24 05:01:16 +0100

    Add simple/crappy latency check that updates every 5 seconds

--- predictive.js
+++ predictive.js
@@ -1,6 +1,7 @@
 
-var UPS = 10;
+var UPS = 1;
 var VEL = 20;
+var latency = {};
 
 var world = [
 	{ x: 10, y: 10, dx: 0, dy: 0 }
@@ -31,10 +32,52 @@ io.sockets.on("connection", function(socket) {
 		{ x: 200, y: 200, dx: 0, dy: 0 }
 	];
 
+	latency[socket.id] = [];
+
 	socket.on("pong", function(response) {
-		var now = new Date().getTime();
-		//console.log("RTT: " + (now - response.stime) + "ms");
-		socket.emit("rtt", now - response.stime);
+		var actual_latency, tmp, i, now = new Date().getTime();
+		latency[socket.id].push(now - response.stime);
+		if( latency[socket.id].length < 5 ) {
+			socket.emit("ping", { stime: now });
+		} else {
+			var samples = latency[socket.id];
+			samples.sort(function(a, b) { 
+				return a - b;
+			});
+
+			var median = samples[2];
+			for( i = 0, mean = 0 ; i < samples.length ; i++ ) {
+				mean += samples[i];
+			}
+
+			mean /= samples.length;
+
+			for( i = 0, std_dev = 0 ; i < samples.length ; i++ ) {
+				tmp = samples[i] - mean;
+				std_dev += (tmp * tmp);
+			}
+
+			std_dev /= (samples.length - 1);
+			std_dev = Math.sqrt(std_dev);
+			
+			for( i = 0, actual_latency = [] ; i < samples.length ; i++ ) {
+				if( (samples[i] > (median - std_dev)) && (samples[i] < (median + std_dev)) ) {
+					actual_latency.push(samples[i]);
+				}
+			}
+
+			var player_latency = 0;
+			for( i = 0 ; i < actual_latency.length ; i++ ) {
+				player_latency += actual_latency[i];
+			}
+
+			player_latency /= actual_latency.length;
+			//console.log(latency, actual_latency, player_latency);
+
+			latency[socket.id] = [];
+
+			socket.emit("rtt", Math.ceil(player_latency / 2));
+		}
 	});
 
 	socket.emit("ping", { stime: new Date().getTime() });
@@ -69,6 +112,12 @@ io.sockets.on("connection", function(socket) {
 setInterval(function() {
 	send_update();
 }, 1000 / UPS);
+
+setInterval(function() {
+	io.sockets.emit("ping", {
+		stime: clock
+	});
+}, 5000);
 
 function send_update() {
 	var new_clock = new Date().getTime();
